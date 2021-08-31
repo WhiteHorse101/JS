acbcc186c2edec17647d03bd4fccaa43fe84a4e1
WhiteHorse101 <rishabh.saudagar2001@gmail.com>
2021-08-31 23:40:26 +0530

    added new folder and made changes to exising code

--- js-basic/Clousers/clouser.js
+++ js-basic/Clousers/clouser.js
@@ -36,3 +36,33 @@ function checkthis(){
 
     console.log(this)
 }
+
+let a = {
+
+    a: 10,
+    b: 'abc',
+    c: true,
+    d: function(){
+
+        console.log(this)
+    },
+    20:800,     //here we have taken number as property but we cannot call it directly because js considers this as a string
+    e: {
+        l:25,
+        m: 'rishabh',
+
+        f:{
+            n:'json',
+            o:100
+
+        }
+    }
+
+}
+
+console.log(a.e.l)
+console.log(a.e.f.n)
+// console.log(a.20) uncomment this, here you will face error because it not the right way of accesing that token 
+
+console.log(a[20]) // here you can see 20 can be accessed this way 
+
